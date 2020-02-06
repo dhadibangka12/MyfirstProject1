f6acbfe033b7540122b1fd43dce69c47c373ff19
dhadiBABEL12 <dhadiisriansyah55@gmail.com>
2020-02-07 02:50:25 +0700

    update pertama kali 1. mengubah main activity 2. menambah warna abu-abu dengan kode #708090

--- app/src/main/java/com/example/myfirstproject1/MainActivity.java
+++ app/src/main/java/com/example/myfirstproject1/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.myfirstproject1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int a = 10;
+
+
+        Button btnPindah = findViewById(R.id.btn);
+        btnPindah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main3Activity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
